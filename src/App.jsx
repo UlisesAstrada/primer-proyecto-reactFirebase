04254ae4ae7b0ba8afce8066c20c2955116a9042
Ulises <ulises_astrada@outlook.com>
2021-02-22 00:25:03 -0300

    Form component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
 import React from 'react';
-import Temperatura from './components/Temperatura';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <div className="App mt-5">
       <h1>Primer proyecto</h1>     
-      <Temperatura />
+      <Formulario />
     </div>
   );
 } 
